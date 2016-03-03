15ab896d0f7ac9964655aed7b578b872cc3ca960
masterUNG <phrombutr@gmail.com>
2016-03-03 10:40:33 +0700

    delete SQLite finish

--- app/src/main/java/appewtc/masterung/chinalearning/MainActivity.java
+++ app/src/main/java/appewtc/masterung/chinalearning/MainActivity.java
@@ -1,5 +1,6 @@
 package appewtc.masterung.chinalearning;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -17,9 +18,18 @@ public class MainActivity extends AppCompatActivity {
         myManage = new MyManage(this);
 
         //Test Add Value
-        testAddValue();
+        //testAddValue();
+
+        //Delete All Data
+        deleteAllData();
 
     }   // Main Method
+
+    private void deleteAllData() {
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        sqLiteDatabase.delete(MyManage.learn_table, null, null);
+    }
 
     private void testAddValue() {
         myManage.addLearn("testUnit", "ง่าย", "urlImage", "Vol", "read", "meaning", "urlSound");
