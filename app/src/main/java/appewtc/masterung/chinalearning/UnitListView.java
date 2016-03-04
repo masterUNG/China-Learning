b3b5c618c7f51c65d560440f4fd5e8f36fe8d897
masterUNG <phrombutr@gmail.com>
2016-03-04 09:37:04 +0700

    Create cursor where unit finish

--- app/src/main/java/appewtc/masterung/chinalearning/UnitListView.java
+++ app/src/main/java/appewtc/masterung/chinalearning/UnitListView.java
@@ -1,5 +1,7 @@
 package appewtc.masterung.chinalearning;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -26,8 +28,35 @@ public class UnitListView extends AppCompatActivity {
         //Show View
         showView();
 
+        //Create ListView
+        createListView();
+
 
     }   // Main Method
+
+    private void createListView() {
+
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM learnTABLE WHERE Unit = " + "'" + unitString + "'", null);
+        cursor.moveToFirst();
+
+        String[] iconStrings = new String[cursor.getCount()];
+        String[] vocabularyStrings = new String[cursor.getCount()];
+
+        for (int i=0;i<cursor.getCount();i++) {
+
+            iconStrings[i] = cursor.getString(cursor.getColumnIndex(MyManage.column_Image));
+            vocabularyStrings[i] = cursor.getString(cursor.getColumnIndex(MyManage.column_Vocabulary));
+
+            cursor.moveToNext();
+        }   //for
+        cursor.close();
+
+        UnitAdapter unitAdapter = new UnitAdapter(UnitListView.this, iconStrings, vocabularyStrings);
+        detailListView.setAdapter(unitAdapter);
+
+    }   // createListView
 
     private void showView() {
 
